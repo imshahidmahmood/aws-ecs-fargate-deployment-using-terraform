09bfdeae1bbd9c9ad95f45d1300ed7ec72c61e99
Shahid Mahmood <shahidmahmood.yar@gmail.com>
2024-09-10 17:10:28 +0500

    removed git history

---
+++ ecs_task_definition.tf
@@ -0,0 +1,36 @@
+resource "aws_ecs_task_definition" "kipina_dev_task" {
+  family                   = var.task_family_name
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.task_cpu
+  memory                   = var.task_memory
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "kipina-dev-container"
+      image     = var.container_image
+      essential = true
+      portMappings = [
+        {
+          containerPort = var.container_port
+          hostPort      = var.container_port
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/${var.task_family_name}"
+          "awslogs-region"        = var.aws_region
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+      environment = [
+        for key, value in jsondecode(file("${path.root}/.env.json")) : {
+          name  = key
+          value = value
+        }
+      ]
+    }
+  ])
+}
